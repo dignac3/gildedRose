81a0a7b31ebf4897a9e001daaf29c79297f81211
Dorian Dignac <dorian.dignac19@hotmail.fr>
2017-11-21 07:23:27 +0100

    Récupération code legacy

---
+++ src/test/java/fr/unilim/iut/gildedRose/InnTest.java
@@ -0,0 +1,13 @@
+package fr.unilim.iut.gildedRose;
+
+import org.junit.Test;
+//import static org.assertj.core.api.Assertions.assertThat;
+
+public class InnTest {
+	@Test
+	public void negative_quality() {
+		Inn items = new Inn();
+		items.updateQuality();
+		
+	}
+}
